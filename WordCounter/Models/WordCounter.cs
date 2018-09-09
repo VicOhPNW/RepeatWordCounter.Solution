d0674b32d64024156749adaf623e87ce22c3ffa5
Victoria Oh <ohvictori@gmail.com>
2018-09-09 16:18:45 -0700

    Add test for GeetFindInput method.

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -5,20 +5,29 @@ namespace WordCounter
 {
   public class RepeatCounter
   {
-    private string _searchInput; //property - user input for List
-    public RepeatCounter (string searchInput) //constructor
+    private string _searchInput; //property - user input for Search
+    private string _findInput; //property - user input for Find
+
+    public RepeatCounter(string searchInput, string findInput) //constructor
     {
       _searchInput = searchInput;
+      _findInput = findInput;
     }
 
-    public string GetSearchInput()
+    public string GetSearchInput() //method for search input validation
     {
       return _searchInput;
     }
 
-    public void SetSearchInput (string newSearchInput)
+    public void SetSearchInput(string newSearchInput)
     {
       _searchInput = newSearchInput;
     }
+
+    public string GetFindInput() //method for find input validation
+    {
+      // return _findInput;
+    }
+
   }
 }
